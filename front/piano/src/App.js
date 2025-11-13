f2c3b74e9e58b8521aefe4d071c96db8820af7fc
Ivan Lartsov <lartsov@gmail.com>
2025-11-13 21:26:38 +0300

    Piano + names

--- front/piano/src/App.js
+++ front/piano/src/App.js
@@ -1,85 +1,138 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function Key(props){
-  console.log(props);
-return (
-<div className = {props.type} onClick={function k () {console.log(props.name);}} />
+function Key(props) {
+	console.log(props);
+	return (
+		<div className={props.type} onClick={() => { props.setText(props.name + props.octave) }} >
+			{/* <p>{props.name}</p> */}
 
-);
+		</div>
+
+	);
 
 }
 
+function Octave(props) {
+	return (
+
+
+		<div className="octave">
+
+			<Key type="white" name="C" octave={props.n} setText={props.setText} />
+			<Key type="black" name="C#" octave={props.n} setText={props.setText} />
+			<Key type="white" name="D" octave={props.n} setText={props.setText} />
+			<Key type="black" name="D#" octave={props.n} setText={props.setText} />
+			<Key type="white" name="E" octave={props.n} setText={props.setText} />
+
+			<Key type="white" name="F" octave={props.n} setText={props.setText} />
+			<Key type="black" name="F#" octave={props.n} setText={props.setText} />
+			<Key type="white" name="G" octave={props.n} setText={props.setText} />
+			<Key type="black" name="G#" octave={props.n} setText={props.setText} />
+			<Key type="white" name="A" octave={props.n} setText={props.setText} />
+			<Key type="black" name="A#" octave={props.n} setText={props.setText} />
+			<Key type="white" name="H" octave={props.n} setText={props.setText} />
+
+
+
+
+		</div>
+
+
+	);
+
+}
+
+
+function Piano(props) {
+	const [text, setText] = useState("");
+	return (
+		<div >
+
+			<div className="letter"><p>{text}</p></div>
+			<div className="piano" >
+
+				<Octave n={1} setText={setText} />
+				<Octave n={2} setText={setText} />
+				<Octave n={3} setText={setText} />
+			</div>
+
+		</div>
+	);
+}
+
+
+
+
 function App() {
-  return (
-    <div className="App">
-
-      
-<nav class="navbar">
-
-        <div class="navdiv">
-
-            
-            <div class="Left_Header">
-                
-                <button class="Main_Button"><a href="/src/index3.php">Диапазон</a></button>
-            </div>
+	return (
+		<div className="App">
 
 
+			<nav class="navbar">
 
-             
-            <div class="Center_Header">
-                  
-                <form method="post">
-
-                    <div type="text" name="text" placeholder="Поиск" class="search-input"/>
-                    <button type="submit" name="search" class="search_button_2"></button>
+				<div class="navdiv">
 
 
-                </form>
-            </div>
+					<div class="Left_Header">
+
+						<button class="Main_Button"><a href="/src/index3.php">Диапазон</a></button>
+					</div>
 
 
 
 
-             
-            <div class="Right_Header">
-                  
-                <ul>
-                     
-                    <li> <a href="/src/index3.php">Главная</a> </li>
-                     
-                    <li> <a href="/src/index3.php?sort=popular">Популярные</a></li>
+					<div class="Center_Header">
 
-                     
-            
-             
-                    
-                        <button>
-                            <a href="profile.php"></a>
-                        </button>
-                        <button>
-                            <a href="auth.php?mode=logout">Выйти</a>
-                        </button>
-                    
-                        <button>
-                            <a href="auth.php?mode=login">Войти</a>
-                        </button>
-                    
-                </ul>
-            </div>
+						<form method="post">
 
-        </div>
-    </nav>
+							<div type="text" name="text" placeholder="Поиск" class="search-input" />
+							<button type="submit" name="search" class="search_button_2"></button>
 
 
+						</form>
+					</div>
+
+
+
+
+
+					<div class="Right_Header">
+
+						<ul>
+
+							<li> <a href="/src/index3.php">Главная</a> </li>
+
+							<li> <a href="/src/index3.php?sort=popular">Популярные</a></li>
+
+
+
+
+
+							<button>
+								<a href="profile.php"></a>
+							</button>
+							<button>
+								<a href="auth.php?mode=logout">Выйти</a>
+							</button>
+
+							<button>
+								<a href="auth.php?mode=login">Войти</a>
+							</button>
+
+						</ul>
+					</div>
+
+				</div>
+			</nav>
 
 
 
 
 
 
-
+			<Piano a={3} />
 
 
 
@@ -91,32 +144,23 @@ function App() {
 
 
 
-      <div className="main">
-
-      <Key type = "white" name = "C" number = {1}   />
-      <Key type = "black" name = "C#" number = {2}   />
-      <Key type = "white" name = "D" number = {3}   />
-      <Key type = "black" name = "D#" number = {4}   />
-      <Key type = "white" name = "E" number = {5}   />
-
-      <Key type = "white" name = "F" number = {6}   />
-      <Key type = "black" name = "F#" number = {7}   />
-      <Key type = "white" name = "G" number = {8}   />
-      <Key type = "black" name = "G#" number = {9}   />
-      <Key type = "white" name = "A" number = {10}   />
-      <Key type = "black" name = "A#" number = {11}   />
-      <Key type = "white" name = "H" number = {12}   />
 
 
 
 
-      </div>
-
-      
 
 
-    </div>
-  );
+
+
+
+
+
+
+
+
+
+		</div>
+	);
 }
 
 export default App;
